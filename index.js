13b25c15fb5c5305adb6f45fa740bc523686ecef
Matthew Would <matthew.would@derivco.co.za>
2017-10-02 14:50:59 +0200

    add paths

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const fetch = require('node-fetch');
-const host = 'https://bittrex.com/api/v1.1/public/getcurrencies';
+const host1 = 'https://bittrex.com/api/v1.1/public/getticker',
+    host2 = 'https://api.coindesk.com/v1/bpi/currentprice/ZAR.json',
+    host3 = 'https://api.mybitx.com/api/1/ticker?pair=BTCZAR';
 
-fetch(host)
+fetch(host2)
     .then(function (res) {
         return res.json();
     }).then(function (json) {
         console.log(Object.keys(json));
-        console.log(json.result[0]);
+        // console.log(json.result[0]);
+        console.log(json);
     });
