7f2d5830bdd8e2e6c7e677e644077d785b3dfe37
macbook <macbook@macbooks-MacBook-Pro.local>
2017-10-02 22:54:48 +0200

    basic query

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 'use strict';
 
 const fetch = require('node-fetch');
-const host1 = 'https://bittrex.com/api/v1.1/public/getticker',
-    host2 = 'https://api.coindesk.com/v1/bpi/currentprice/ZAR.json',
-    host3 = 'https://api.mybitx.com/api/1/ticker?pair=BTCZAR';
+const exchanges = [
+    {
+        exchange: 'Coindesk',
+        locale: 'US',
+        path: 'https://api.coindesk.com/v1/bpi/currentprice/ZAR.json',
+        adapter: {
+            apiPath: ['bpi', 'ZAR', 'rate_float']
+        }
+    },
+    {
+        exchange: 'Luno',
+        locale: 'SA',
+        path: 'https://api.mybitx.com/api/1/ticker?pair=XBTZAR',
+        adapter: {
+            apiPath: ['last_trade']
+        }
+    }
+],
+    promises = [];
 
-fetch(host2)
-    .then(function (res) {
-        return res.json();
-    }).then(function (json) {
-        console.log(Object.keys(json));
-        // console.log(json.result[0]);
-        console.log(json);
+exchanges.forEach((exchangeSettings) => promises.push(getRate(exchangeSettings)));
+Promise.all(promises).then((result) => console.log(`${exchanges[0].exchange} ${exchanges[0].locale} => ${exchanges[1].locale} Arbitrage: ${result[1] - result[0]}`));
+
+function getRate(settings) {
+    return new Promise((resolve, rej) => {
+        fetch(settings.path)
+            .then(function (res) {
+                return res.json();
+            }).then(function (json) {
+                const rate = navigateApiPath(json, settings.adapter.apiPath);
+
+                console.log(`${settings.exchange} ${settings.locale} rate: ${rate}`);
+
+                resolve(rate);
+            }).catch(function(err) {
+                console.log("-- API ERROR --");
+            });;
     });
+}
+
+function navigateApiPath(json, apiPath) {
+    let location = json;
+
+    apiPath.forEach((element) => {
+        location = location[element]
+    });
+
+    return location;
+}
